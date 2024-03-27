89b7365522fe9d0be783cc9ddfb5360fd1f0a723
matt <55065240+mattbal@users.noreply.github.com>
2024-03-27 13:18:49 -0700

    added popover panel, set default status to applied

--- src/app/components/AddJobBtn.tsx
+++ src/app/components/AddJobBtn.tsx
@@ -16,9 +16,11 @@ import {
   Heading,
 } from 'react-aria-components';
 import Status from './Status';
+import { Oval } from 'react-loader-spinner';
 
 export default function AddJobBtn() {
   const [jobURL, setJobURL] = useState('');
+  const [status, setStatus] = useState('react-aria-1');
 
   return (
     <DialogTrigger>
@@ -35,9 +37,9 @@ export default function AddJobBtn() {
         </svg>
         Add job
       </Button>
-      <ModalOverlay className='fixed inset-0 z-20 flex items-center justify-center min-h-full overflow-y-auto bg-black/25'>
-        <Modal className='w-full max-w-sm p-6 overflow-hidden text-left align-middle bg-white rounded shadow-xl'>
-          <Dialog className='relative outline-none'>
+      <ModalOverlay className='fixed inset-0 z-20'>
+        <Modal className='w-full max-w-sm p-6 overflow-hidden text-left align-middle bg-white shadow-xl absolute right-0 inset-y-0 border-l border-gray-200'>
+          <Dialog className=''>
             {({ close }) => (
               <form className='flex flex-col'>
                 <div className='flex items-center pb-2 border-b border-gray-950/10'>
@@ -61,18 +63,48 @@ export default function AddJobBtn() {
                     </svg>
                   </Button>
                 </div>
-                <TextField autoFocus className='mt-5'>
+                <TextField autoFocus className='mt-5' value={jobURL} onChange={setJobURL}>
                   <Label>Job post URL</Label>
-                  <Input
-                    placeholder='https://...'
-                    value={jobURL}
-                    onChange={(e) => setJobURL(e.target.value)}
-                    className='text-gray-700 appearance-none rounded block px-2.5 py-3 w-full ring-1 ring-gray-950/20 focus:ring-inset  focus:ring-2 focus:ring-blue-600 focus:outline-none hover:ring-gray-950/30 mt-2'
-                  />
+                  <div className='flex mt-2 rounded'>
+                    <Input
+                      placeholder='https://...'
+                      className='text-gray-700 appearance-none block px-2.5 py-3 w-full ring-1 ring-inset ring-gray-950/20 focus:ring-2 focus:ring-blue-600 focus:outline-none rounded-l'
+                    />
+                    <Button
+                      className='px-3.5 py-2 text-white -ml-px bg-blue-500 w-fit active:bg-blue-600 rounded-r'
+                      onPress={() => alert('test')}
+                    >
+                      Scan
+                    </Button>
+                  </div>
                 </TextField>
-                <Select className='flex flex-col mt-4'>
-                  <Label>Job status</Label>
-                  <Button className='flex items-center max-w-fit border border-gray-950/20 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-600 py-3 px-2.5 text-gray-700 hover:bg-gray-100 hover:border-gray-950/30 rounded mt-2'>
+                <TextField className='mt-4'>
+                  <Label>Name</Label>
+                  <Input className='text-gray-700 appearance-none block px-2.5 py-3 w-full ring-1 ring-inset ring-gray-950/20 focus:ring-2 focus:ring-blue-600 focus:outline-none hover:ring-gray-950/30 rounded-l' />
+                </TextField>
+                <TextField className='mt-4'>
+                  <Label>Company</Label>
+                  <Input className='text-gray-700 appearance-none block px-2.5 py-3 w-full ring-1 ring-inset ring-gray-950/20 focus:ring-2 focus:ring-blue-600 focus:outline-none hover:ring-gray-950/30 rounded-l' />
+                </TextField>
+                <TextField className='mt-4'>
+                  <Label>Job ID</Label>
+                  <Input className='text-gray-700 appearance-none block px-2.5 py-3 w-full ring-1 ring-inset ring-gray-950/20 focus:ring-2 focus:ring-blue-600 focus:outline-none hover:ring-gray-950/30 rounded-l' />
+                </TextField>
+                <TextField className='mt-4'>
+                  <Label>Date Posted</Label>
+                  <Input className='text-gray-700 appearance-none block px-2.5 py-3 w-full ring-1 ring-inset ring-gray-950/20 focus:ring-2 focus:ring-blue-600 focus:outline-none hover:ring-gray-950/30 rounded-l' />
+                </TextField>
+                <TextField className='mt-4'>
+                  <Label>Date Applied</Label>
+                  <Input className='text-gray-700 appearance-none block px-2.5 py-3 w-full ring-1 ring-inset ring-gray-950/20 focus:ring-2 focus:ring-blue-600 focus:outline-none hover:ring-gray-950/30 rounded-l' />
+                </TextField>
+                <Select
+                  selectedKey={status}
+                  onSelectionChange={(selected) => setStatus(selected)}
+                  className='flex flex-col mt-4'
+                >
+                  <Label>Status</Label>
+                  <Button className='flex items-center max-w-fit border border-gray-950/20 focus:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-blue-600 py-3 px-2.5 text-gray-700 rounded mt-2'>
                     <SelectValue className='flex-1' />
                     <svg
                       xmlns='http://www.w3.org/2000/svg'
@@ -90,7 +122,7 @@ export default function AddJobBtn() {
                     </svg>
                   </Button>
                   <Popover className='overflow-auto bg-white rounded shadow-lg ring-1 ring-black/5'>
-                    <ListBox className='p-1 outline-none'>
+                    <ListBox selectionMode='single' className='p-1 outline-none'>
                       <ListBoxItem className='flex items-center p-1 text-gray-900 rounded select-none group'>
                         <Status>Applied</Status>
                       </ListBoxItem>
