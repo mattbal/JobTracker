9d9dfc144720f66639c34ec9b0778704a8ca01ea
matt <55065240+mattbal@users.noreply.github.com>
2024-04-08 12:53:53 -0700

    fixed rounded bug

--- src/app/components/AddJobBtn.tsx
+++ src/app/components/AddJobBtn.tsx
@@ -149,7 +149,7 @@ export default function AddJobBtn() {
                               id='jobURL'
                               value={jobURL}
                               onChange={(e) => setJobURL(e.target.value)}
-                              className='block w-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 rounded-l'
+                              className='block w-full border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 rounded'
                               placeholder='https://'
                             />
                           </div>
